9910bedcdc6d52abd693205834392ecdf18ff56a
Synnysyn <69205616+Synnysyn@users.noreply.github.com>
2021-03-15 21:13:55 +0100

    first configuration

---
+++ charitydonation/charitydonation/example_local_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "charity-donation",
+        "HOST": "localhost",
+        "PASSWORD": "coderslab",
+        "USER": "postgres",
+        "PORT": 5432,
+    }
+}
+
+# Rename this file to "local_settings.py" after configuration!
